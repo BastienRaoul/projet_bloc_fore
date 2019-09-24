53fe831928a254a39f19717221108e650d5f7380
Bastien RAOUL <bastien.raoul@etu.univ-nantes.fr>
2019-09-24 11:40:59 +0200

    regex done

--- src/projetIUT_blocFore/Main.java
+++ src/projetIUT_blocFore/Main.java
@@ -1,13 +1,10 @@
 package projetIUT_blocFore;
 
-import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class Main {
 	private static Scanner input;
@@ -21,7 +18,7 @@ public class Main {
 	public static void readFile() throws FileNotFoundException {
 		try {
 //          System.out.print("Enter the file name with extension : ");            
-            input = new Scanner("/home/e174687c/RÃ©seau/Perso/Documents/LP MiAR/projet_bloc_fore/src/projetIUT_blocFore/TextSample.txt");
+            input = new Scanner("C:\\Users\\basti\\OneDrive\\Documents\\LP MiAR\\projet_bloc_fore\\src\\projetIUT_blocFore\\TextSample.txt");
             file = new File(input.nextLine());    
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -32,30 +29,43 @@ public class Main {
 		input = new Scanner(file);
 		ArrayList<String> lignes = new ArrayList<String>();
 		ArrayList<String> resultat = new ArrayList<String>();
+        Boolean face1 = false, face2 = false, face3 = false, face4 = false;
 		
 		while (input.hasNextLine()) { lignes.add(input.nextLine()); }
-		
-        if(!Pattern.compile("([0-9]+\\s*[;]\\s*){2}[0-9]+").matcher(lignes.get(0)).find()) {
+
+        if(!Pattern.compile("([0-9]+\\s*[;]\\s*){2}[0-9]+$").matcher(lignes.get(0)).find()) {
         	resultat.add("Erreur de syntaxe -> " + lignes.get(0));
         }
         
-        Boolean face = false;
 		for (String ligne : lignes) {
-			if(face) {
-				if(Pattern.compile("[A-Z]+[0-9]").matcher(ligne).find()) {
-					if (!Pattern.compile("([0-9]+\\s*[,]\\s*){4}[0-9]+").matcher(ligne).find()){
+			if(!Pattern.compile("[A-Z]+[0-9]$").matcher(ligne).find()) {
+				if(!Pattern.compile("([0-9]+[,]\\s){4}[0-9]+$").matcher(ligne).find()) {
+					if(!Pattern.compile("([0-9]+\\s*[;]\\s*){2}[0-9]+$").matcher(ligne).find()) {
 						resultat.add("Erreur de syntaxe -> " + ligne);
 					}
-				} else if (!Pattern.compile("([0-9]+\\s*[,]\\s*){4}[0-9]+").matcher(ligne).find()){
-					resultat.add("Erreur de syntaxe -> " + ligne);
 				}
-			} else {
-				if(Pattern.compile("[A-Z]+[0-9]").matcher(ligne).find()) {
-					face = true;
-				} else {
-					resultat.add("Erreur de syntaxe -> " + ligne);
-				}
-			}
+			}else if(Pattern.compile("FACE1").matcher(ligne).find()) {
+				face1 = true;
+			} else if(Pattern.compile("FACE2").matcher(ligne).find()) {
+				face2 = true;
+			} else if(Pattern.compile("FACE3").matcher(ligne).find()) {
+				face3 = true;
+			} else if(Pattern.compile("FACE4").matcher(ligne).find()) {
+				face4 = true;
+			}	
+			
+		}
+		if(!face4) {
+			resultat.add(0, "Face 4 non déclaré");
+		}
+		if(!face3) {
+			resultat.add(0, "Face 3 non déclaré");
+		} 
+		if(!face2) {
+			resultat.add(0, "Face 2 non déclaré");
+		}
+		if(!face1) {
+			resultat.add(0, "Face 1 non déclaré");
 		}
 		
 		System.out.println(resultat);
