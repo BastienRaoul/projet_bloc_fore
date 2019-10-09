df36e20a7efd5baa2b5436fa6167e8e3c5622f54
HappyKendhal <remithomas2602@gmail.com>
2019-10-10 00:04:54 +0200

    bastien remi regrouped

--- src/projetIUT_blocFore/Drilling.java
+++ src/projetIUT_blocFore/Drilling.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Drilling {
-
+	
+	private Block block;
 	private Face face;
 	private float[] faceCoords = new float[3];
 	private float[] absCoords = new float[3];
 	private float depth;
 	private float diameter;
-	private List<Drilling> testedDrillings = new ArrayList<Drilling>();
-
-	public Drilling(Face face, float[] faceCoords, float depth, float diameter) {
+	private List<Drilling> testedDrillingsIntersects = new ArrayList<Drilling>(); // With which drillings this one has been tested
+	
+	public Drilling(Block block, Face face, float[] faceCoords, float depth, float diameter) {
+		this.block = block;
 		this.face = face;
 		this.faceCoords = faceCoords;
 		this.depth = depth;
@@ -20,9 +22,74 @@ public class Drilling {
 		absCoords = convertFaceToAbsCoords();
 	}
 
+	
 	private float[] convertFaceToAbsCoords() {
 		float[] coords = faceCoords;
-
+		float[] dimBlock = block.getDimensions();
+		
+		switch(face.getId()) {
+		// face 0 ignored cause it's the same coordinates
+		// Right
+		case 1: 
+			coords[0] = dimBlock[0] - faceCoords[2];
+			//coords[1] = faceCoords[1];
+			coords[2] = faceCoords[0];
+			break;
+		// Back
+		case 2: 
+			coords[0] = dimBlock[0] - faceCoords[0];
+			//coords[1] = faceCoords[1];
+			coords[2] = dimBlock[2] - faceCoords[2];
+			break;
+		// Left
+		case 3: 
+			coords[0] = faceCoords[2];
+			//coords[1] = faceCoords[1];
+			coords[2] = dimBlock[0] - faceCoords[0];
+			break;
+		// Top
+		case 4: 
+			//coords[0] = faceCoords[0];
+			coords[1] = dimBlock[1] - faceCoords[2];
+			coords[2] = faceCoords[1];
+			break;
+		// Bottom
+		case 5:
+			//coords[0] = faceCoords[0];
+			coords[1] = faceCoords[2];
+			coords[2] = dimBlock[2] - faceCoords[1];
+			break;
+		}
+		
 		return coords;
+	}
+	
+	@Override
+	public String toString() {
+		return ("(" + faceCoords[0] + ", " + faceCoords[1] + ", " + faceCoords[2] + ") on " + face.toString());
+	}
+	
+	public float[] getFaceCoords() {
+		return faceCoords;
+	}
+
+	public float[] getAbsCoords() {
+		return absCoords;
+	}
+
+	public float getDepth() {
+		return depth;
+	}
+
+	public float getDiameter() {
+		return diameter;
+	}
+
+	public List<Drilling> getTestedDrillingsIntersects() {
+		return testedDrillingsIntersects;
+	}
+
+	public void setTestedDrillingsIntersects(List<Drilling> testedDrillingsIntersects) {
+		this.testedDrillingsIntersects = testedDrillingsIntersects;
 	}
 }
--- src/projetIUT_blocFore/Face.java
+++ src/projetIUT_blocFore/Face.java
@@ -6,32 +6,98 @@ import java.util.List;
 import javax.sound.sampled.LineListener;
 
 public class Face {
-
+	
+	private int MIN_DRILL_ESPACEMENT = 1; // in millimeters
+	
 	private static int id = 0;
 	private float[] dimensions;
 	private int[] rotation = new int[3];
 	private List<Drilling> drillings = new ArrayList<Drilling>();
-
+	
 	public Face(float dimX, float dimY, int rotX, int rotY, int rotZ) {
 		id++;
-		dimensions = new float[] { dimX, dimY };
-		rotation = new int[] { rotX, rotY, rotZ };
+		dimensions = new float[] {dimX, dimY};
+		rotation = new int[] {rotX, rotY, rotZ};
 	}
-
-	public void addDrilling(Drilling drilling) {
-		drillings.add(drilling);
+	
+	public void addDrillings(ArrayList<Drilling> drillings) {
+		drillings.addAll(drillings);
 	}
-
+	
+	public String verifyCoplanarDrillings() {
+		String errors = "";
+		
+		errors += verifyEspacement();
+		errors += verifyDepth();
+		
+		return errors;
+	}
+	
+	private String verifyEspacement() {
+		String errors = "";
+		
+		for (int i = 0; i < drillings.size(); i++) {
+			Drilling drilling = drillings.get(i);
+			
+			// Verify espacement with edges
+			if (drilling.getFaceCoords()[0] - drilling.getDiameter()/2 < MIN_DRILL_ESPACEMENT ||
+					drilling.getFaceCoords()[1] - drilling.getDiameter()/2 < MIN_DRILL_ESPACEMENT) {
+				errors += "[Espacement] " + drilling.toString();
+			}
+			
+			// Verify espacement with other drillings
+			for (int j = 0; j < drillings.size(); j++) {
+				if (i != j) {
+					Drilling otherDrill = drillings.get(j);
+					float centersEspacement = (float) Math.sqrt(
+							Math.pow(drilling.getFaceCoords()[0] - otherDrill.getFaceCoords()[0], 2) +
+							Math.pow(drilling.getFaceCoords()[1] - otherDrill.getFaceCoords()[1], 2)
+					);
+					float drillingsEspacement = centersEspacement - drilling.getDiameter()/2 - otherDrill.getDiameter()/2;
+					if (drillingsEspacement < MIN_DRILL_ESPACEMENT) {
+						errors += "[Espacement] " + drilling.toString() + " with " + otherDrill.toString();
+					}
+				}
+			}
+		}
+		
+		return errors;
+	}
+	
+	private String verifyDepth() {
+		String errors = "";
+		
+		for (int i = 0; i < drillings.size(); i++) {
+			Drilling drilling = drillings.get(i);
+			if (drilling.getFaceCoords()[2] > 0) { // If a drilling inside the block
+				if (!hasParentDrilling(drilling)) {
+					errors += drilling.toString() + " starts inside the block without \"parent drilling\"";
+				}
+			}
+		}
+		
+		return errors;
+	}
+	
+	private boolean hasParentDrilling(Drilling drilling) {
+		
+		return false;
+	}
+	
 	@Override
 	public String toString() {
-		return "Face " + id;
+		return "FACE " + ((char) 65 + id);
 	}
-
+	
 	public List<Drilling> getDrillings() {
 		return drillings;
 	}
-
+	
 	public int[] getRotation() {
 		return rotation;
+	}
+	
+	public int getId() {
+		return id;
 	}
 }
