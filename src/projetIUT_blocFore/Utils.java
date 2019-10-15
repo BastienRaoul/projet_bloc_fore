1e866d296d9a4a55559186aa3452b62ce95d554e
HappyKendhal <remithomas2602@gmail.com>
2019-10-15 11:25:49 +0200

    Tests passed : verify drillings from the same face

--- src/projetIUT_blocFore/Utils.java
+++ src/projetIUT_blocFore/Utils.java
@@ -3,6 +3,7 @@ package projetIUT_blocFore;
 public class Utils {
 
 	public static float getDistance(float x1, float y1, float x2, float y2) {
+		//System.out.println("sqrt( (" + x1 + "-" + x2 + ")^2 + (" + );
 		return (float) Math.sqrt( Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) );
 	}
 	
